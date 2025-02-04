10f0a2e009e199b978db634065d42814e203d0c1
Michal Gajda <michal@gajda.co.uk>
2025-02-04 12:18:42 +0000

    Code Cleanup

--- src/WebApi/Controllers/WeatherForecastController.cs
+++ src/WebApi/Controllers/WeatherForecastController.cs
@@ -22,17 +22,19 @@ public sealed partial class WeatherForecastController(ILogger<WeatherForecastCon
     ];
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<IEnumerable<WeatherForecast>> GetAsync(CancellationToken cancellationToken = default)
     {
         this.LogMessage(RANGE);
 
-        return Enumerable.Range(1, RANGE).Select(index => new WeatherForecast
+        var result = Enumerable.Range(1, RANGE).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
+
+        return await Task.FromResult(result);
     }
 
     [LoggerMessage(LogLevel.Information, Message = "Ping {range}")]
